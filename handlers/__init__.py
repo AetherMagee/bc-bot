bbdca24eba695fb15120b7da92719b452aa55602
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-10-08 18:59:45 +0300

    feat: /pidor and /pidoreg

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -6,3 +6,4 @@ from .layout import *
 from .remind import *
 from .start import *
 from .stats import *
+from .pidor import create_pidors_database, pidor, pidoreg
---
+++ handlers/pidor/__init__.py
@@ -0,0 +1,3 @@
+from .db import create_pidors_database
+from .pidoreg import pidoreg
+from .pidor import pidor
